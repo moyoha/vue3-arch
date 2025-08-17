883c169a3c110c30ba53f73e18e5f36eabf42a4a
mayoha <mayoha@163.com>
2025-08-18 01:18:42 +0800

    toRefs proxyRefs 实现

--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -45,3 +45,44 @@ function triggerRefValue(ref) {
     triggerEffects(dep);
   }
 }
+
+class ObjectRefImpl {
+  public __v_isRef = true;
+  constructor(public _object, public _key) {}
+
+  get value() {
+    return this._object[this._key];
+  }
+  set value(newValue) {
+    this._object[this._key] = newValue;
+  }
+}
+
+export function toRef(object, key) {
+  return new ObjectRefImpl(object, key);
+}
+
+export function toRefs(object) {
+  const res = {};
+  for (const key in object) {
+    res[key] = toRef(object, key);
+  }
+  return res;
+}
+
+export function proxyRefs(objectWithRef) {
+  return new Proxy(objectWithRef, {
+    get(target, key, receiver) {
+      const res = Reflect.get(target, key, receiver);
+      return res.__v_isRef ? res.value : res;
+    },
+    set(target, key, value, receiver) {
+      const oldValue = target[key];
+      if (oldValue.__v_isRef) {
+        oldValue.value = value;
+        return true;
+      }
+      return Reflect.set(target, key, value, receiver);
+    }
+  });
+}
